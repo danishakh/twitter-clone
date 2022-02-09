df8e86402ba6c5f76054c91112dd784746de8e6d
Danish Akhtar <danishakh@gmail.com>
2022-02-08 20:32:25 -0800

    added tweet textarea and emoji pickers. completed ui for tweet component

---
+++ components/Input.js
@@ -0,0 +1,115 @@
+import {
+  PhotographIcon,
+  XIcon,
+  CalendarIcon,
+  ChartBarIcon,
+  EmojiHappyIcon,
+} from '@heroicons/react/outline'
+import React, { useState, useRef } from 'react'
+import { Picker } from 'emoji-mart'
+import 'emoji-mart/css/emoji-mart.css'
+
+const Input = () => {
+  const [input, setInput] = useState('')
+  const [selectedFile, setSelectedFile] = useState(null)
+  const [showEmojis, setShowEmojis] = useState(false)
+  const filePickerRef = useRef(null)
+
+  const addImageToPost = () => {}
+
+  const addEmoji = (e) => {
+    let sym = e.unified.split('-')
+    let codesArray = []
+    sym.forEach((el) => codesArray.push('0x' + el))
+    let emoji = String.fromCodePoint(...codesArray)
+    setInput(input + emoji)
+  }
+
+  return (
+    <div
+      className={`flex space-x-3 overflow-y-scroll border-b border-gray-700 p-3`}
+    >
+      <img
+        src="https://lh3.googleusercontent.com/a/AATXAJwCsuneWAkKlHwMPxOmLNjFACEvbtN8QPwbUsZ-=s96-c"
+        className="h-11 w-11 cursor-pointer rounded-full"
+      />
+      <div className="w-full divide-y divide-gray-700">
+        <div className={`${selectedFile && 'pb-7'} ${input && 'space-y-2.5'}`}>
+          <textarea
+            className="min-h-[50px] w-full bg-transparent text-lg tracking-wide text-[#d9d9d9] placeholder-gray-500 outline-none"
+            value={input}
+            placeholder="Whats happening?"
+            rows="2"
+            onChange={(e) => setInput(e.target.value)}
+          />
+          {selectedFile && (
+            <div className="relative">
+              <div
+                className="absolute top-1 left-1 flex h-8 w-8 cursor-pointer items-center 
+              justify-center rounded-full bg-[#15181c] bg-opacity-75 hover:bg-[#272c26]"
+                onClick={() => setSelectedFile(null)}
+              >
+                <XIcon className="h-5 text-white" />
+              </div>
+              <img
+                src={selectedFile}
+                alt=""
+                className="max-h-80 rounded-2xl object-contain"
+              />
+            </div>
+          )}
+        </div>
+
+        <div className="flex items-center justify-between pt-2.5">
+          <div className="flex items-center">
+            <div className="icon" onClick={() => filePickerRef.current.click()}>
+              <PhotographIcon className="h-[22px] text-[#1d9bf0]" />
+              <input
+                type="file"
+                onChange={addImageToPost}
+                ref={filePickerRef}
+                hidden
+              />
+            </div>
+
+            <div className="icon rotate-90">
+              <ChartBarIcon className="h-[22px] text-[#1d9bf0]" />
+            </div>
+
+            <div className="icon" onClick={() => setShowEmojis(!showEmojis)}>
+              <EmojiHappyIcon className="h-[22px] text-[#1d9bf0]" />
+            </div>
+
+            <div className="icon">
+              <CalendarIcon className="h-[22px] text-[#1d9bf0]" />
+            </div>
+
+            {showEmojis && (
+              <Picker
+                onSelect={addEmoji}
+                style={{
+                  position: 'absolute',
+                  marginTop: '465px',
+                  marginLeft: -40,
+                  maxWidth: '320px',
+                  borderRadius: '20px',
+                }}
+                theme="dark"
+              />
+            )}
+          </div>
+          <button
+            className="disabled:hover:[#1d9bf0] rounded-full bg-[#1d9bf0] px-4 py-1.5 font-bold text-white
+          shadow-md hover:bg-[#1a8cd8] disabled:cursor-default disabled:opacity-50"
+            disabled={!input.trim() && !selectedFile}
+            // onClick={sendPost}
+          >
+            Tweet
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Input
